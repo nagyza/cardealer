4ed32a9d100d343050095f48abec83fe7aa9af35
Zsolt Nagy <nagyza@gmail.com>
2023-08-17 17:07:34 +0200

    Add repository

--- src/main/java/org/nagyza/cardealer/controller/GreetingController.java
+++ src/main/java/org/nagyza/cardealer/controller/GreetingController.java
@@ -1,15 +1,28 @@
 package org.nagyza.cardealer.controller;
 
 import org.nagyza.cardealer.model.Car;
+import org.nagyza.cardealer.repository.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 public class GreetingController {
 
+    CarRepository carRepository;
+
+    @Autowired
+    public GreetingController(CarRepository carRepository) {
+        this.carRepository = carRepository;
+    }
+
     @GetMapping(path = "/hello")
-    public String getGreeting() {
-        Car car = new Car(1L,"Toyota", "Corolla");
-        return "Hello user \n You can buy a " + car.getBrand() + " " + car.getType();
+    public List<Car> getGreeting() {
+
+        return carRepository.findAll();
     }
 }
